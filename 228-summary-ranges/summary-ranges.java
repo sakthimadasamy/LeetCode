255016bf771f52ddf0299c90551c48ce9500fb8c
Sakthi Madasamy. M <sakthimadasamy78@gmail.com>
2026-09-10 10:59:14 +0530

    Time: 3 ms (77.06%) | Memory: 43.3 MB (21.58%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+
+        List<String> result = new ArrayList<>();
+
+        if (nums.length == 0) {
+            return result;
+        }
+
+        int start = nums[0];
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            if (nums[i] + 1 != nums[i + 1]) {
+
+                if (start == nums[i]) {
+                    result.add(String.valueOf(start));
+                } else {
+                    result.add(start + "->" + nums[i]);
+                }
+
+                start = nums[i + 1];
+            }
+        }
+
+        // Add the last range
+        if (start == nums[nums.length - 1]) {
+            result.add(String.valueOf(start));
+        } else {
+            result.add(start + "->" + nums[nums.length - 1]);
+        }
+
+        return result;
+    }
+}
