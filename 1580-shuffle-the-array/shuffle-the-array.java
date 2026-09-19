51c62232ad9f69f48659e2082a513e53ee54df41
Sakthi Madasamy. M <sakthimadasamy78@gmail.com>
2026-09-19 15:04:32 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (22.07%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans=new int[nums.length];
+        int k=0;
+        for(int i=0;i<n;i++)
+        {
+            ans[k]=nums[i];
+            k++;
+            ans[k]=nums[i+n];
+            k++;
+        }
+        return ans;
+        
+    }
+}
